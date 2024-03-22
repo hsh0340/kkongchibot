76edec1dce132dc56ab2d3b5126c679d36aec897
hsh0340 <hsh0340@naver.com>
2024-03-22 15:35:22 +0900

    config: eslint & prettier 설정

--- src/smilegate/smilegate.ts
+++ src/smilegate/smilegate.ts
@@ -1,14 +1,18 @@
-import {getAllCharacterProfiles} from "./api";
-import {CharacterProfile} from "./types";
+import { getAllCharacterProfiles } from './api';
+import { CharacterProfile } from './types';
 
-export async function getOneCharacterProfile(characterName: string): Promise<CharacterProfile> {
+export async function getOneCharacterProfile(
+  characterName: string,
+): Promise<CharacterProfile> {
   const allCharacterProfiles = await getAllCharacterProfiles(characterName);
 
   if (!allCharacterProfiles) {
     throw new Error();
   }
 
-  const characterProfile = allCharacterProfiles.find(characterProfile => characterProfile.CharacterName === characterName);
+  const characterProfile = allCharacterProfiles.find(
+    (characterProfile) => characterProfile.CharacterName === characterName,
+  );
 
   if (!characterProfile) {
     throw new Error();
