8daa884a3ea298c2050589d18692d0f8c0cb1d9d
hsh0340 <hsh0340@naver.com>
2024-04-01 16:25:51 +0900

    feat: 캐릭터 정보 return 하는 command 생성

--- src/smilegate/smilegate.ts
+++ src/smilegate/smilegate.ts
@@ -1,22 +1,29 @@
 import { getAllCharacterProfiles } from './api';
 import { CharacterProfile } from './types';
 
-export async function getOneCharacterProfile(
-  characterName: string,
-): Promise<CharacterProfile> {
-  const allCharacterProfiles = await getAllCharacterProfiles(characterName);
+// export async function getOneCharacterProfile(
+//   characterName: string,
+// ): Promise<CharacterProfile> {
+//   const allCharacterProfiles = await getAllCharacterProfiles(characterName);
+//
+//   if (!allCharacterProfiles) {
+//     throw new Error();
+//   }
+//
+//   const characterProfile = allCharacterProfiles.find(
+//     (characterProfile) => characterProfile.CharacterName === characterName,
+//   );
+//
+//   if (!characterProfile) {
+//     throw new Error();
+//   }
+//
+//   return characterProfile;
+//
+//   /**
+//    * object 형태로 넘어오는 거 가공
+//    *
+//    */
+// }
 
-  if (!allCharacterProfiles) {
-    throw new Error();
-  }
 
-  const characterProfile = allCharacterProfiles.find(
-    (characterProfile) => characterProfile.CharacterName === characterName,
-  );
-
-  if (!characterProfile) {
-    throw new Error();
-  }
-
-  return characterProfile;
-}
--- src/smilegate/types.ts
+++ src/smilegate/types.ts
@@ -12,3 +12,33 @@ export interface CharacterProfile {
   // 아이템 최대 레벨
   ItemMaxLevel: string;
 }
+
+export interface Embed {
+  color?: number;
+  title?: string;
+  url?: string;
+  author: {
+    name: string;
+    icon_url: string;
+    url: string;
+  };
+  description?: string;
+  thumbnail?: {
+    url: string;
+  };
+  fields?: {
+    name: string;
+    value: string;
+    inline?: boolean;
+  }[];
+  image?: {
+    url: string;
+    width?: number;
+    height?: number;
+  };
+  timestamp?: string;
+  footer?: {
+    text: string;
+    icon_url: string;
+  };
+}
